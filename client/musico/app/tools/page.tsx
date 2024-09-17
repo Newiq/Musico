2a67ef9ced368e1d75b7be2eced65835c1d5d744
Newiq <784751939@qq.com>
2024-09-17 11:35:56 -0400

    tools page structure

--- client/musico/app/tools/page.tsx
+++ client/musico/app/tools/page.tsx
@@ -1,8 +1,39 @@
 export default function ToolPage() {
     return (
-    <div className="min-h-screen bg-beige-100">
-    <main className="container mx-auto p-4">
-        <h1>tool Page</h1>
+    <div className="min-h-screen">
+    <main className="container mx-auto p-4 ">
+        <h1 className="text-5xl font-bold mt-6 mb-6 w-full text-center">Music Tools</h1>
+        <div className="flex w-full flex-col lg:flex-row justify-center">
+        <div className="card lg:card-side bg-base-100 shadow-xl">
+        <figure>
+            <img
+            src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
+            alt="Album" />
+        </figure>
+        <div className="card-body">
+            <h2 className="card-title">New album is released!</h2>
+            <p>Click the button to listen on Spotiwhy app.</p>
+            <div className="card-actions justify-end">
+            <button className="btn btn-primary">Listen</button>
+            </div>
+        </div>
+        </div>
+        <div className="divider lg:divider-horizontal">🎧</div>
+        <div className="card lg:card-side bg-base-100 shadow-xl">
+            <figure>
+                <img
+                src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
+                alt="Album" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">New album is released!</h2>
+                <p>Click the button to listen on Spotiwhy app.</p>
+                <div className="card-actions justify-end">
+                <button className="btn btn-primary">Listen</button>
+                </div>
+            </div>
+            </div>
+</div>
     </main>
     </div>
 );
